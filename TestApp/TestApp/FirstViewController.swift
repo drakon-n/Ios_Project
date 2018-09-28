34ac41110a4f35408b74c823fdc3dbc62108d843
Drakon_n <ttviimail@mail.ru>
2018-09-28 23:29:33 +0300

    test app
    just an empty app on swift

---
+++ TestApp/TestApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestApp
+//
+//  Created by Влад on 28.09.2018.
+//  Copyright © 2018 Влад. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
