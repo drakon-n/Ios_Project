2aa8021cdc7e1f76f1e8b325df36200f45a6eec9
Drakon_n <ttviimail@mail.ru>
2018-11-07 23:29:48 +0300

    Json Parser
    Json parser project added

--- TestApp/TestApp/FirstViewController.swift
+++ TestApp/TestApp/FirstViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    @IBOutlet weak var FloorField: UITextField!
+    @IBOutlet weak var LessonField: UITextField!
+    @IBOutlet weak var TimeField: UITextField!
+    @IBOutlet weak var BraveBar: UISegmentedControl!
+    
+   
+    @IBAction func MainButton(_ sender: Any) {
+        print("Hello World!")
+       // if(FloorField==nil)||(LessonField==nil)||(TimeField==nil){print("Недостаточно данных")}
+       // else{
+       // let floor = Double(FloorField.text!)
+       // let lesson = Double(LessonField.text!)
+        //    let time = Double(TimeField.text!)}
+        //let res = (6-lesson)*3+2+(floor-3)*2
+    
+    
 
 }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     //   Do any additional setup after loading the view, typically from a nib.
+    }
 
+}
